afeee2032370c38f0d239bf329ab5d2a07fed565
jspinerola <144980273+jspinerola@users.noreply.github.com>
2026-05-03 16:54:17 -0500

    fixed train script

--- train_v2_model.sh
+++ train_v2_model.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# V2 Script to only run jobs that failed last time due to path issue
-#SBATCH --job-name=vulnllm_eval
+#SBATCH --job-name=js_lora_train
 #SBATCH --partition=gpu          
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -8,9 +7,8 @@
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:a30:2         
 #SBATCH --time=06:00:00
-#SBATCH --output=results/hprc_eval_%j.log
+#SBATCH --output=results/training_v2_%j.log
 
-# 1. SETUP ENVIRONMENT (Placeholders for TAMU HPRC)
 # 1. SETUP ENVIRONMENT (Placeholders for TAMU HPRC)
 module load GCC/12.3.0 CUDA/12.1.1 Anaconda3/2023.09-0
 eval "$(conda shell.bash hook)"
